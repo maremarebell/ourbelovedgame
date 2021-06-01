83f7c2e65f64e3506c30dd470ee372e815796ab3
Maryann Bell <mabell@adobe.com>
2021-06-01 09:13:02 -0400

    Additional player edits

--- src/Player.js
+++ src/Player.js
@@ -5,7 +5,7 @@ import { Status } from "./Status";
 
 export const Player = (props) => {
 
-  const { name, age, job, location, player_status, slug, name_full, season_stats } = props.singlePlayerData;
+  const { name, age, job, location, player_status, slug, name_full, season_stats, social_media, gor } = props.singlePlayerData;
 
   return (
     <>
@@ -14,7 +14,7 @@ export const Player = (props) => {
 
           <div className="player-header__pic col col-1">
 
-            <img className="player-header__headshot" src={`../${slug}.png`}></img>
+            <img className="player-header__headshot" src={`../player-${slug}.png`}></img>
           </div>
 
           <div className="player-header__info col col-2">
@@ -31,9 +31,9 @@ export const Player = (props) => {
               <li>{location}</li>
             </ul>
 
-            <a className="social-icon social-icon--instagram" href="https://www.instagram.com/aaronrclancy/" target="_blank">
-              <img className="social-icon__icon" height="0" src="logo-instagram.svg" />
-              <span className="social-icon__text">aaronrclancy</span>
+            <a className="social-icon social-icon--instagram" href={`https://www.instagram.com/${social_media.instagram_handle}`} target="_blank">
+              <img className="social-icon__icon" height="0" src="../logo-instagram.svg" />
+              <span className="social-icon__text">{social_media.instagram_handle}</span>
               <div className="tooltip">instagram</div>
             </a>
           </div>
@@ -59,17 +59,17 @@ export const Player = (props) => {
                   </div>
                   <div className="stat">
                     <span className="stat__title">Hujus</span>
-                    <span className="stat__value">0</span>
+                    <span className="stat__value">{season_stats.hujus}</span>
                     <span className="stat_detail"></span>
                   </div>
                   <div className="stat">
                     <span className="stat__title">PTCs</span>
-                    <span className="stat__value">0</span>
+                    <span className="stat__value">{season_stats.ptcs}</span>
                     <span className="stat_detail"></span>
                   </div>
                   <div className="stat">
                     <span className="stat__title">Love Level</span>
-                    <span className="stat__value">0</span>
+                    <span className="stat__value">{season_stats.love_level}</span>
                     <span className="stat_detail"></span>
                   </div>
                 </div>
@@ -78,26 +78,29 @@ export const Player = (props) => {
               <div className="gor-review">
                 <h2 className="gor-review__title">GoR Pre-Game Assessment</h2>
 
-               
-                <p className="gor-review__callout">Listen to Aaron's assessment at <span className="gor-review__timestamp">5:49</span> (1:25:23 remaining)</p>
+                <p className="gor-review__callout">
+                  Listen to {name}'s assessment at &nbsp;
+                  <span className="gor-review__timestamp">{gor.gor_assessment_timestamp}</span>&nbsp;
+                  ({gor.gor_assessment_timestamp_remaining} remaining)
+                </p>
 
                 <div className="gor-review__predictions">
                   <h3 className="predictions__title">Pre-Season Predictions</h3>
 
-                  <img className="gor-review__ball" src="image-ball.png"/>
+                  <img className="gor-review__ball" src="../image-ball.png"/>
 
                   <ul className="predictions">
                     <li className="prediction">
-                      <span className="prediction__attribute">Limo exit:</span>
-                      Shirtless
+                      <span className="prediction__attribute">Limo exit: </span>
+                      {gor.predictions.limo_exit}
                     </li>
                     <li className="prediction">
-                      <span className="prediction__attribute">Gameplay:</span>
-                      Will take shirt off
+                      <span className="prediction__attribute">Gameplay: </span>
+                      {gor.predictions.gameplay}
                     </li>
                     <li className="prediction">
-                      <span className="prediction__attribute">Game length:</span>
-                      Mid-season floater
+                      <span className="prediction__attribute">Game length: </span>
+                      {gor.predictions.game_length}
                     </li>
                   </ul>
                 </div>
@@ -107,24 +110,31 @@ export const Player = (props) => {
             <div className="col col-1 column--secondary-info">
 
               <ul className="secondary-social__links">
+
+                {social_media.tiktok_handle.length > 0 &&
                 <li className="secondary-social__link">
-                  <a className="" href="#" target="_blank">
-                    <img className="secondary-social__icon" src="logo-tiktok.svg" height="0"/>
-                    <span className="secondary-social__label">Tiktok: N/A</span>
+                  <a href={`${social_media.tiktok_url}`} target="_blank">
+                    <img className="secondary-social__icon" src="../logo-tiktok.svg" height="0"/>
+                    <span className="secondary-social__label">Tiktok: {social_media.tiktok_handle}</span>
                   </a>
                 </li>
+                }
+
                 <li className="secondary-social__link">
-                  <a className="" href="https://abc.com/shows/the-bachelorette/cast/aaron-summer-2021" target="_blank">
-                    <img className="secondary-social__icon" src="logo-abc.svg" height="0" />
+                  <a href={`${social_media.abc_profile}`} target="_blank">
+                    <img className="secondary-social__icon" src="../logo-abc.svg" height="0" />
                     <span className="secondary-social__label">Official ABC Bio</span>
                   </a>
                 </li>
+
+                {social_media.linkedin_url.length > 0 &&
                 <li className="secondary-social__link">
-                  <a className="" href="https://www.linkedin.com/in/aaron-clancy-217bb111a/" target="_blank">
-                    <img className="secondary-social__icon" src="logo-linkedin.svg" height="0" />
-                    <span className="secondary-social__label">LinkedIn: Account Executive at Health IQ</span>
+                  <a href={`${social_media.linkedin_url}`} target="_blank">
+                    <img className="secondary-social__icon" src="../logo-linkedin.svg" height="0" />
+                    <span className="secondary-social__label">LinkedIn: {social_media.linkedin_job}</span>
                   </a>
                 </li>
+                }
               </ul>
 
               <div className="tags">
