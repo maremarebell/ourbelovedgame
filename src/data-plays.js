f6607d20186f4295c807058e016e6484d9fd1225
Maryann Bell <mabell@adobe.com>
2021-06-18 09:39:51 -0400

    Outline for new player data

---
+++ src/data-plays.js
@@ -0,0 +1,133 @@
+export const playerPlaysData = [
+  {
+    name: "Template"
+    plays: [
+      {
+        type: "",
+        title: "",
+        details: "",
+        episode: "",
+      },
+    ]
+  },
+  {
+    name: "Aaron",
+    plays: [],
+  },
+  {
+    name: "Andrew M",
+    plays: [],
+  },
+  {
+    name: "Andrew S",
+    plays: [],
+  },
+  {
+    name: "Austin",
+    plays: [],
+  },
+  {
+    name: "Blake",
+    plays: []
+  },
+  {
+    name: "Brandon",
+    plays: []
+  },
+  {
+    name: "Brendan",
+    plays: []
+  },
+  {
+    name: "Christian",
+    plays: []
+  },
+  {
+    name: "Cody",
+    plays: []
+  },
+  {
+    name: "Connor B",
+    plays: []
+  },
+  {
+    name: "Conor C",
+    plays: []
+  },
+  {
+    name: "David",
+    plays: []
+  },
+  {
+    name: "Gabriel",
+    plays: []
+  },
+  {
+    name: "Garrett",
+    plays: []
+  },
+  {
+    name: "Greg",
+    plays: []
+  },
+  {
+    name: "Hunter",
+    plays: []
+  },
+  {
+    name: "Jeff",
+    plays: []
+  },
+  {
+    name: "John",
+    plays: []
+  },
+  {
+    name: "Josh",
+    plays: []
+  },
+  {
+    name: "Justin",
+    plays: []
+  },
+  {
+    name: "Karl",
+    plays: []
+  },
+  {
+    name: "Kyle",
+    plays: []
+  },
+  {
+  name: "Landon",
+    plays: []
+  },
+    {
+    name: "Marcus",
+    plays: []
+  },
+  {
+    name: "Marty",
+    plays: []
+  },
+  {
+    name: "Michael",
+    plays: []
+  },
+  {
+    name: "Mike",
+    plays: []
+  },
+  {
+    name: "Quartney",
+    plays: []
+  },
+  {
+    name: "Thomas",
+    plays: []
+  },
+  {
+    name: "Tre",
+    plays: []
+  },
+];
