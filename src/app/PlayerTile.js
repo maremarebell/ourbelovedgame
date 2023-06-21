313554fa6cf5c21a6b589c9ba76b53ef8149b887
Maryann Bell <maremarebell@gmail.com>
2023-06-21 10:46:25 -0400

    Fixed all missing asset issues on homepage

--- src/app/PlayerTile.js
+++ src/app/PlayerTile.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Status } from "./Status";
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 export const PlayerTile = (props) => {
 
+  const imagePath = `/assets/players/player-${props.data.slug}.png`;
+
   return (
     <li className={`tile all-players__player--${props.data.player_status}`}>
-      <img className="tile__headshot" src={`../player-${props.data.slug}.png`} alt={`headshot of ${props.data.name}`}></img>
-
+      <Image
+        src={imagePath}
+        alt={`headshot of ${props.data.name}`}
+        className="tile__headshot"
+        height={120}
+        width={120}
+      />
       <div className="tile__info">
         <Link className="tile__name" href={`/player/${props.data.slug}`}>
           {props.data.name}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,46 @@ import React from "react";
 import { playerData } from "./data";
 import { PlayerTile } from "./PlayerTile";
 
+interface Player {
+  player_status: string;
+}
+
 export default function Players() {
+  // Group the players based on player_status
+  const groupedPlayers = playerData.reduce<{ [key: string]: Player[] }>((groups, player) => {
+    const { player_status } = player;
+    if (!groups[player_status]) {
+      groups[player_status] = [];
+    }
+    groups[player_status].push(player);
+    return groups;
+  }, {});
+
+  // Define the order of groups
+  const groupOrder = ["active", "unannounced", "eliminated", "sidelined"];
+
   return (
     <>
       <div className="player-container">
-        {/* <h2 className="update-notice">Website last updated: 6/21 9:30AM ET right before Episode 3</h2> */}
+        <h2 className="update-notice">Website last updated: 6/21 9:30AM ET right before Episode 3</h2>
         <h1 className="all-player__title">UNDER CONSTRUCTION!</h1>
-        
-        {/* <ul className="all-players__list">
-          {playerData.map((data, key) => (
-            <PlayerTile data={data} key={key} />
+
+        <ul className="all-players__list">
+          {groupOrder.map((status) => (
+            <React.Fragment key={status}>
+              {groupedPlayers[status] && (
+                <div className={`players-group ${status}`}>
+                  <ul className="all-players__list">
+                    {groupedPlayers[status].map((player, index) => (
+                      <PlayerTile key={index} data={player} />
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </React.Fragment>
           ))}
-        </ul> */}
+        </ul>
       </div>
     </>
   );
-};
+}
