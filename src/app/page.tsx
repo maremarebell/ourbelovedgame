18bae9d9533742c2bedd06f51867a8c08e9ee3ca
Maryann Bell <maremarebell@gmail.com>
2023-06-20 21:59:31 -0400

    Oh and these files too lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,22 @@
 'use client';
 
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Players } from './Players';
-import { Player } from "./Player";
+import React from "react";
 import { playerData } from "./data";
-import { useParams } from "react-router-dom";
+import { PlayerTile } from "./PlayerTile";
 
-interface PlayerData {
-  name: string;
-  name_full: string;
-  slug: string;
-  age: string;
-  job: string;
-  location: string;
-  origin: string;
-  tags: string[];
-  player_status: string;
-  player_status_last_update: string;
-  gor?: {
-    gor_assessment_epi?: string;
-    gor_assessment_timestamp?: string;
-    predictions: {
-      limo_exit?: string;
-      gameplay?: string;
-      game_length?: string;
-      notes?: string;
-    };
-  };
-  season_stats: {
-    season: string;
-    roses: number;
-    hujus: number;
-    ptcs: number;
-  };
-  social_media: {
-    instagram_url: string;
-    instagram_handle: string;
-    tiktok_url: string;
-    tiktok_handle: string;
-    instagram_scrubbed: boolean;
-    abc_profile: string;
-    linkedin_url: string;
-    linkedin_job: string;
-  };
-}
-
-const PersonPage: React.FC = () => {
-  const { slug } = useParams<{ slug: string }>();
-
-  const player = playerData.find((player: PlayerData) => player.slug === slug);
-
-  return (
-    <div className="App">
-      {player ? <Player data={player} /> : <p>Player not found</p>}
-    </div>
-  );
-};
-
-const HomePage = () => {
-  return (
-    <div className="App">
-      <Players />
-    </div>
-  );
-};
-
-const App = () => {
+export default function Players() {
   return (
     <>
-      <Router>
-        <Fragment>
-          {/* <ScrollToTop /> */}
-          <Routes>
-            <Route path='/' element={<HomePage/>} />
-            <Route path='/player/:slug' element={<PersonPage />} />
-          </Routes>
-        </Fragment>
-      </Router>
+      <div className="player-container">
+        <h2 className="update-notice">Website last updated: 6/21 9:30AM ET right before Episode 3</h2>
+        <h1 className="all-player__title">Players of the Game</h1>
+        
+        <ul className="all-players__list">
+          {playerData.map((data, key) => (
+            <PlayerTile data={data} key={key} />
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
-
-export default App;
