0cbba7dc4fbdfbf97e8bfad9ac0f793789e080eb
Maryann Bell <maremarebell@gmail.com>
2023-06-22 20:07:16 -0400

    Cleaned up some files

--- src/data/data-terms.js
+++
@@ -1,7 +0,0 @@
-export const gameTerms = [
-	{
-		term: "ITM",
-		definition_short: "In the Moment (ITM); On-camera confessionals",
-		definition_long: "On-camera confessionals during which contestants tell viewers (read: producers) how they’re feeling about a given situation, fellow contestant, or even the bachelor/bachelorette him/herself.",
-	}
-]
