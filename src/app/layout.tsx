ad192b563c15c454f7ed2384e6ec178549affa43
Maryann Bell <maremarebell@gmail.com>
2023-06-25 21:54:13 -0400

    lol idk maybe this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,15 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:type" content="article" />
-        <meta property="og:image" content="/og-charity.png" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Our Beloved Game 🌹" />
-        <meta name="twitter:description" content="Check out the men of Katie's Season 17 of the Bachelorette." />
-        <meta name="twitter:image" content="https://ourbelovedgame.com/twitter-charity.png" />
-      </Head>
       <body suppressHydrationWarning={true} className={inter.className}>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import playerData from "./data-20.json";
 import { PlayerTile } from "./PlayerTile";
 import Link from 'next/link';
 import { Schibsted_Grotesk } from 'next/font/google'
+import Head from 'next/head';
 
 const sgfont = Schibsted_Grotesk({ subsets: ['latin'] })
 
@@ -28,23 +29,34 @@ export default function Players() {
   const groupOrder = ["active", "unannounced", "eliminated", "sidelined"];
 
   return (
-    <div className="container">
-      <h1 className={sgfont.className}>Season 20 Players</h1>
-      <p className="update-notice">Last updated: 6/22 6:42PM ET 2023</p>
+    <>
+      <Head>
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="/og-charity.png" />
 
-      <ul className="players">
-        {groupOrder.map((status) => (
-          <React.Fragment key={status}>
-            {groupedPlayers[status] && groupedPlayers[status].map((player, index) => (
-              <PlayerTile key={index} data={player} index={index} />
-            ))}
-          </React.Fragment>
-        ))}
-      </ul>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Our Beloved Game 🌹" />
+        <meta name="twitter:description" content="Check out the men of Katie's Season 17 of the Bachelorette." />
+        <meta name="twitter:image" content="https://ourbelovedgame.com/twitter-charity.png" />
+      </Head>
+      <div className="container">
+        <h1 className={sgfont.className}>Season 20 Players</h1>
+        <p className="update-notice">Last updated: 6/22 6:42PM ET 2023</p>
 
-      <Link href="/season-17" className="previous-season">
-        Check out previous players of Season 17
-      </Link>
-    </div>
+        <ul className="players">
+          {groupOrder.map((status) => (
+            <React.Fragment key={status}>
+              {groupedPlayers[status] && groupedPlayers[status].map((player, index) => (
+                <PlayerTile key={index} data={player} index={index} />
+              ))}
+            </React.Fragment>
+          ))}
+        </ul>
+
+        <Link href="/season-17" className="previous-season">
+          Check out previous players of Season 17
+        </Link>
+      </div>
+    </>
   );
 }
