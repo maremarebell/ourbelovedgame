e2865a648fbaf736b4fe46380084d5b40ce1306d
Maryann Bell <maremarebell@gmail.com>
2023-08-04 12:26:47 -0400

    Added Season 20 Episode 6 data

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,5 +1,5 @@
 export async function fetchEpisodesData() {
-  const episodeNumbers = [1, 2, 3, 4, 5];
+  const episodeNumbers = [1, 2, 3, 4, 5, 6];
   console.log('well ya got here')
   try {
     const episodesData = await Promise.all(
